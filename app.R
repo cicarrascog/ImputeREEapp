44e5639a7c88d94b550102a98982d22a1dd8f873
cicarrascog <41179053+cicarrascog@users.noreply.github.com>
2022-09-08 15:39:13 +1000

    Update app.R
    Added Contents, added imputation controls, added corrections for all elements

--- app.R
+++ app.R
@@ -17,6 +17,8 @@ library(bs4Dash)
 `%notin%` <- Negate(`%in%`)
 
 
+# UI Section ---------
+
 header <- bs4DashNavbar(
   title = dashboardBrand(
     title = "ImputeREEapp",
@@ -27,6 +29,7 @@ header <- bs4DashNavbar(
   skin = "dark"
 )
 
+## sidebar pages definition ------
 sidebar <- dashboardSidebar(
   title = "ImputeREEapp",
   sidebarMenu(
@@ -39,54 +42,56 @@ sidebar <- dashboardSidebar(
     menuItem(text = "Contact", tabName = "Contact")
   )
 )
-
+## Content of pages ------
 body <- dashboardBody(
   tabItems(
+    ### Instruction Tab ------
     tabItem(
       tabName = "Instructions",
-      box(includeMarkdown("README.md"), width = 8)
+      box(includeMarkdown("Instructions.md"), width = 8)
     ),
+    ### Upload Tab ------
     tabItem(
       tabName = "Upload",
       fluidRow(
         box(title = HTML('<b>Upload</b>'),
-          width = 5,
-          headerBorder = F,
-          collapsible = F,
-          solidHeader = T,
-          label = boxLabel(text = "Help", status = "info", tooltip = "Upload a CSV file"),
-          fileInput(
-            inputId = "newFile", # upload file ####
-            label = NULL,
-            accept = c(".csv"),
-            buttonLabel = "Upload...",
-            placeholder = "a .csv file"
-          ),
-          radioButtons(
-            inputId = "chondrite_values", # Choose chondrite values ####
-            label = "Chondrite Values",
-            choices = c(
-              `Palme and O'Neill (2014)` = "PalmeOneill2014CI",
-              `McDonough and Sun (1995)` = "McDonough1995CI"
+            width = 5,
+            headerBorder = F,
+            collapsible = F,
+            solidHeader = T,
+            label = boxLabel(text = "Help", status = "info", tooltip = "Upload a CSV file"),
+            fileInput(
+              inputId = "newFile",
+              label = NULL,
+              accept = c(".csv"),
+              buttonLabel = "Upload...",
+              placeholder = "a .csv file"
+            ),
+            radioButtons(
+              inputId = "chondrite_values",
+              label = "Chondrite Values",
+              choices = c(
+                `Palme and O'Neill (2014)` = "PalmeOneill2014CI",
+                `McDonough and Sun (1995)` = "McDonough1995CI"
+              )
             )
-          )
         ),
         box(title = HTML('<b>Model variables</b>'),
-          width = 7,
-          headerBorder = F,
-          collapsible = F,
-          solidHeader = T,
-          HTML('Selected elements are used for modelling. '),
-          textOutput("Rees"), # Choose chondrite values ####
-          p(""), ### space lol
-          checkboxGroupInput(
-            inputId = "NormalizeMethod",
-            label = "Chondrite Values",
-            inline = T,
-            choices = REE_plus_Y_Elements,
-            selected = REE_plus_Y_Elements[REE_plus_Y_Elements %notin%
-              c("La", "Ce", "Eu", "Y")]
-          )
+            width = 7,
+            headerBorder = F,
+            collapsible = F,
+            solidHeader = T,
+            HTML('Selected elements are used for modelling. '),
+            textOutput("Rees"),
+            p(""),
+            checkboxGroupInput(
+              inputId = "NormalizeMethod",
+              label = "Chondrite Values",
+              inline = T,
+              choices = REE_plus_Y_Elements,
+              selected = REE_plus_Y_Elements[REE_plus_Y_Elements %notin%
+                                               c("La", "Ce", "Eu", "Y")]
+            )
         )
         ## here another box
       ),
@@ -125,9 +130,7 @@ body <- dashboardBody(
           solidHeader = T,
           sidebar = boxSidebar(
             id = "correct_heavy_sliders",
-            sliderInput(inputId = "Y", label = "Y correction", value = 1.29, min = 0.1, max = 2),
-            sliderInput(inputId = "Yb", label = "Yb correction", value = 1/0.8785 , min = 0.1, max = 2), ## 1 / 0.8785
-            sliderInput(inputId = "Lu", label = "Lu correction", value = 1/0.8943 , min = 0.1, max = 2),
+            HTML("R<sup>2</sup> filter. Evaluate correction only for models R<sup>2</sup> > 0.9"),
             sliderInput(
               inputId = "rsquared_filter",
               label = "R squared filter",
@@ -135,17 +138,37 @@ body <- dashboardBody(
               min = 0,
               max = 2,
               step = 0.001
-            )##  1 / 0.8943
+            ),
+
+            #### Correction Factor Sliders -----------
+            HTML("This sliders are correction factors to accound for differences between measured and calculated values."),
+            sliderInput(inputId = "Pr", label = "Pr correction", value = 1/0.918503242748121, min = 0.1, max = 2),
+            sliderInput(inputId = "Nd", label = "Nd correction", value = 1/0.964163780696138 , min = 0.1, max = 2),
+            sliderInput(inputId = "Sm", label = "Sm correction", value = 1/1.03474021036956 , min = 0.1, max = 2),
+            sliderInput(inputId = "Gd", label = "Gd correction", value = 1/1.08353863509898, min = 0.1, max = 2),
+            sliderInput(inputId = "Tb", label = "Tb correction", value =  1/1.12453248979803 , min = 0.1, max = 2),
+            sliderInput(inputId = "Dy", label = "Dy correction", value = 1/1.13717202794484 , min = 0.1, max = 2),
+            sliderInput(inputId = "Ho", label = "Ho correction", value = 1/1.06914181064329, min = 0.1, max = 2),
+            sliderInput(inputId = "Er", label = "Er correction", value = 1/0.982456334111282 , min = 0.1, max = 2),
+            sliderInput(inputId = "Tm", label = "Tm correction", value = 1/0.973645612359077 , min = 0.1, max = 2),
+            sliderInput(inputId = "Yb", label = "Yb correction", value = 1/0.848963529664848 , min = 0.1, max = 2),
+            sliderInput(inputId = "Lu", label = "Lu correction", value = 1/0.774442796976892 , min = 0.1, max = 2),
+            sliderInput(inputId = "Y", label = "Y correction", value = 1/0.713075166441028, min = 0.1, max = 2),
+
+
+
+
           )
         ) ## here another box
       ) ,
       fluidRow(
         box(title = HTML('<b>Summary statistics</b>'),
-          width = 12,
+            width = 12,
             reactableOutput('ratio_table')
-            )
-        )## here another fluidrow
+        )
+      )## here another fluidrow
     ),
+    ### Data Tab ------
     tabItem(
       tabName = "Data",
       fluidRow(
@@ -180,59 +203,77 @@ body <- dashboardBody(
         )
       )
     ),
+    ### Download Tab ------
     tabItem(
       tabName = "Download",
-      tooltip(
-        title = "Should imputed values be included in the Output?",
-        placement = "right",
-        tag = checkboxInput(
-          inputId = "impute",
-          label = "Impute?",
-          value = FALSE
-        )
+      HTML('<h2>Description of Downloaded Data to be added</h2>'),
+      box(title = HTML('<h3>Impute Values?</h3>'),
+          HTML('Ticking this option will add a column for each REE in the format <code>Imputed_REEname</code>,
+             where missing values are replaced for those calculated from this regression. Measured values are kept.'),
+          HTML('The R<sup>2</sup> slider, allows to set a threshold for imputation. Models with a fitting lower than the threshold are not imputed'),
+          sliderInput(inputId = 'R_filter_export',label = HTML("R<sup>2</sup> filter"), value = 0.9, min = 0.01, max = 1,step = 0.01  ),
+          tooltip(
+            title = "Should imputed values be included in the Output?",
+            placement = "right",
+            tag = checkboxInput(
+              inputId = "impute",
+              label = "Impute?",
+              value = FALSE
+            )
+          )
+
       ),
-      tooltip(
+      box(title = HTML('<h3>Include Chondrite nomalized values?</h3>'),
+          HTML('Ticking this option will add a column for each REE in the format <code>Normalized_REEname</code> and <code>NormalizedCalc_REEname</code> to include the Chondrite normalized values using measured and calculated concentrations, respectively.'),
+
+        tooltip(
         title = "Should Chondrite normalized values be included in the Output?",
         placement = "right",
         tag = checkboxInput(
           inputId = "include_norm_values",
           label = "Include Chondrite Normalized Values?",
           value = FALSE
-        )
+        ))
+
       ),
-      downloadButton(outputId = "donwload_data")
+
+      box(Title = HTML('<h2>Click below to download</h2>'),
+        downloadButton(outputId = "donwload_data")
+      )
+
     ),
+    ### References Tab ------
     tabItem(
       tabName = "References",
       box(includeMarkdown('Reference.md'), width = 9)
     ),
+    ### Changelog Tab ------
     tabItem(
       tabName = "Changelog",
       box(includeMarkdown('Changelog.md'), width = 9)
     ),
     tabItem(
+      ### Contact Tab ------
       tabName = "Contact",
       box(includeMarkdown('Contact.md'), width = 9)
     )
-
   )
 )
 
 
 
 
-####### ui ############
+# UI function ############
 ui <- dashboardPage(header, sidebar, body, title = "ImputeREEapp", help = T)
 
-
+# server Section -----------
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   thematic::thematic_shiny() ## makes plots to match html theme
 
-  # Check_names <- reactive({
-  #
-  # })
 
+
+  ## Load Testing data ------
   sample_data <- reactive({
     testing_data %>%
       # slice(1:500) %>%
@@ -241,7 +282,7 @@ server <- function(input, output) {
   })
 
 
-  ### MODEL REEE #########
+  ## Prepare Data ------
 
   base_data <- reactive({
 
@@ -272,7 +313,7 @@ server <- function(input, output) {
 
 
     normalized_data <- data %>% imputeREE:::Element_norm(method = !!sym(input$chondrite_values))
-
+    ### Model Section Data ------
     withProgress(
       message = "Fitting Models",
       data <- data %>% model_REE(., exclude = REE_to_model, method = !!sym(input$chondrite_values), correct_heavy = F)
@@ -291,18 +332,30 @@ server <- function(input, output) {
 
 
 
-
+  ## Apply Corrections  ------
   corrected_data <- reactive({
     data <- base_data() %>% imputeREE:::correct_heavy(
       Y_correction_fact = input$Y,
       Yb_correction_fact = input$Yb,
-      Lu_correction_fact = input$Lu
-    )
+      Lu_correction_fact = input$Lu,
+      Ho_correction_fact = input$Ho,
+      Er_correction_fact = input$Er,
+      Tm_correction_fact = input$Tm
+  ) %>%  imputeREE:::correct_middle(
+    Nd_correction_fact = input$Nd,
+    Sm_correction_fact = input$Sm,
+    Gd_correction_fact = input$Gd,
+    Tb_correction_fact = input$Tb,
+    Dy_correction_fact = input$Dy,
+    Pr_correction_fact = input$Pr
+
+  )
 
     return(data)
   })
 
-  ###  Create long format with normalized values
+  ##  Create long format with normalized values ----------------
+
   norm_table <- reactive({
     data <- corrected_data() %>%
       select(rowid, `R.squared` , matches("Normalized")) %>%
@@ -318,28 +371,37 @@ server <- function(input, output) {
     return(list(data_long, data))
   })
 
+
+## This is the data used for the table that is shown in Data tab ----------------
+
   modelled_data <- reactive({
-    data <- norm_table()[[2]] %>%  pivot_wider(id_cols = 'rowid', names_from = 'Element_name', names_prefix = 'Ratio_', values_from = 'Ratio')
+    data <- norm_table()[[2]] %>%
+      pivot_wider(id_cols = 'rowid', names_from = 'Element_name', names_prefix = 'Ratio_', values_from = 'Ratio')
+
     data <- left_join(corrected_data(),data,by ='rowid') %>%
+      mutate(`Ce/Ce*` = Ce/ppmCalc_Ce ,
+      `Eu/Eu*` = Eu/ppmCalc_Eu ) %>%
       relocate(
         rowid, model_nree,
         R.squared,
         adj.R.squared,
         Intercept,
         Slope,
+        `Ce/Ce*`,
+        `Eu/Eu*`,
         REE_plus_Y_Elements,
         matches("_Normalized$"), matches("^ppmCalc_"), matches("^NormalizedCalc_"), matches("^Ratio_")
       )
   })
 
 
-  ######### Excluded REE ######
+  ## List of excluded REE ----------------
   output$Rees <- renderText({
     paste("Excluded from model:", paste(" ", REE_plus_Y_Elements[REE_plus_Y_Elements %notin% input$NormalizeMethod], collapse = " , "))
   })
-  ### Tables ###############
+  ## tables ----------------
 
-  ####### render reactable with all the data ##############
+  ###  reactable for the results ##############
 
   output$Result <- renderReactable({
     data <- modelled_data()
@@ -352,24 +414,24 @@ server <- function(input, output) {
     # }
 
     reactable(data %>%  select(-dplyr::last_col(15):-last_col()),
-      defaultPageSize = 25,
-      resizable = T,
-      showPageSizeOptions = T,
-      defaultColDef = colDef(format = reactable::colFormat(digits = 3)),
-      columns = list(
-        rowid = colDef(name = "ID", format = colFormat(digits = 0))
+              defaultPageSize = 25,
+              resizable = T,
+              showPageSizeOptions = T,
+              defaultColDef = colDef(format = reactable::colFormat(digits = 3)),
+              columns = list(
+                rowid = colDef(name = "ID", format = colFormat(digits = 0))
 
-      ),
-      highlight = T,
-      onClick = "select",
-      selection = "single",
-      filterable = T,
-      defaultSelected = 1,
-      theme = reactablefmtr::flatly()
+              ),
+              highlight = T,
+              onClick = "select",
+              selection = "single",
+              filterable = T,
+              defaultSelected = 1,
+              theme = reactablefmtr::flatly()
     )
   })
 
-  ####### summary table #########
+  ###  Summary table in upload ##############
   output$REE <- renderReactable({
     req(getReactableState("Result")$selected)
     id_num <- getReactableState("Result")$selected
@@ -380,14 +442,14 @@ server <- function(input, output) {
     data <- norm_table()[[2]] %>% filter(rowid == id_num)
     reactable(data,
               resizable = T,
-      # showPageSizeOptions = T,
-      # defaultPageSize = 8,
-      defaultColDef = colDef(format = reactable::colFormat(digits = 3)),
-      highlight = T,
-      theme = reactablefmtr::flatly(),
-      columns = list(
-        rowid = colDef(name = "ID", format = colFormat(digits = 0))
-      ),
+              # showPageSizeOptions = T,
+              # defaultPageSize = 8,
+              defaultColDef = colDef(format = reactable::colFormat(digits = 3)),
+              highlight = T,
+              theme = reactablefmtr::flatly(),
+              columns = list(
+                rowid = colDef(name = "ID", format = colFormat(digits = 0))
+              ),
     )
   })
 
@@ -500,24 +562,26 @@ server <- function(input, output) {
       ggplot(aes(y = Ratio, x = forcats::fct_inorder(Element_name))) +
       geom_boxplot(
         # aes(fill = forcats::fct_inorder(Element_name))
-        ) +
+      ) +
       geom_hline(yintercept = 1, lty = 2, alpha = 0.8)+
       # scale_fill_viridis_d() +
       theme(legend.position = 'none')+
       scale_y_log10(
         # labels = scales::label_number(accuracy = 0.00001)
-        )
+      )
   })
 
 
   download_data <- reactive(
     {
       data <- modelled_data()
+
+
       if (input$include_norm_values == FALSE) {
         data <- data %>% select(!matches("^NormalizedCalc|Normalized"))
       }
       if (input$impute) {
-        data <- data %>% impute_REE()
+        data <- data %>% impute_REE(rsquared = input$R_filter_export)
       }
       return(data)
     }
